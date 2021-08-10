76b9db062b8bd5d6ab27ceb9037b4b4dea61be2f
Assault-8448 <vvvvvgaga12356@yahoo.co.jp>
2021-08-10 16:16:22 +0900

    add: ffmpeg/frame_interpolation.sh

---
+++ ffmpeg/frame_interpolation.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+ffmpeg -i "input_file" -vf minterpolate=60:2:0:1:8:16:32:0:1:5 output.mp4
+
+# input_file は目的のmp4等に変更。
+
+# minterpolate の値は必要に応じて変更する必要があるっぽい。
+# 基本的に「60」はFPSで、それ以外は変えなくてもよさそう？
+# コマンドは「fps, mi_mode, mc_mode, me_mode, me, mb_size, search_param, vsbmc, scd, scd_threshold」の順番で並んでいる。
+# このうち「vsbmc」は必要に応じて「1」にした方がいいのかも？
+#
+# 参考: https://nico-lab.net/minterpolate_with_ffmpeg/ and https://ffmpeg.org/ffmpeg-filters.html#minterpolate
