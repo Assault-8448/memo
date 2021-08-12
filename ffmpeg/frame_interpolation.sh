efec27a6b0e9ff649d81b224a91ea6e587a1acc6
Assault-8448 <vvvvvgaga12356@yahoo.co.jp>
2021-08-13 03:24:24 +0900

    fix: ffmpeg/frameinterpolation, miss arg

--- ffmpeg/frame_interpolation.sh
+++ ffmpeg/frame_interpolation.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-ffmpeg -i "input_file" -vf minterpolate=60:2:0:1:8:16:32:0:1:5 encoder=h264_nvenc output.mp4
+ffmpeg -i "input_file" -vf minterpolate=60:2:0:1:8:16:32:0:1:5 -vcodec h264_nvenc output.mp4
 
 # input_file は目的のmp4等に変更。
 
@@ -8,7 +8,7 @@ ffmpeg -i "input_file" -vf minterpolate=60:2:0:1:8:16:32:0:1:5 encoder=h264_nven
 # コマンドは「fps, mi_mode, mc_mode, me_mode, me, mb_size, search_param, vsbmc, scd, scd_threshold」の順番で並んでいる。
 # このうち「vsbmc」は必要に応じて「1」にした方がいいのかも？
 #
-# encoder=h264_nvenc は NVIDIA GPU を使用している際に「NVEnc」を利用してエンコードするオプション。
+# -vcodec h264_nvenc は NVIDIA GPU を使用している際に「NVEnc」を利用してエンコードするオプション。
 # WSL だったり、NVIDIA の GPU を使用していない（あるいは使用できない）環境だとエラーを吐くので記述を削除。
 #
 # 参考: https://nico-lab.net/minterpolate_with_ffmpeg/ and https://ffmpeg.org/ffmpeg-filters.html#minterpolate
